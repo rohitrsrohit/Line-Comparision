437c2165b3dc39a000e9264017df5a04a7d22e83
Rohit Patro <rohitpatro6@gmail.com>
2021-07-12 20:55:03 +0530

    Added:UC2 Equals Added

--- src/linecomparision/linecomparision.java
+++ src/linecomparision/linecomparision.java
@@ -9,6 +9,10 @@ public class linecomparision {
 	    x1=1;y1=1;x2=4;y2=4;
 	    dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));	 	    
             System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
+        if(x2-x1 == y2-y1)
+        	System.out.println("x2-x1 equals y2-y1");
+        else
+        	System.out.println("x2-x1 not equals y2-y1");
 
 	}
 
