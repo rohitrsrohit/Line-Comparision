3bf25a7bd7304dae2ab5c7e94fab80aa953dfea0
Rohit Patro <rohitpatro6@gmail.com>
2021-07-12 19:59:00 +0530

    Added:UC1 Cartesian Added

--- src/linecomparision/linecomparision.java
+++ src/linecomparision/linecomparision.java
@@ -4,6 +4,11 @@ public class linecomparision {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Line Comparision");
+		int x1,x2,y1,y2;
+	    double dis;
+	    x1=1;y1=1;x2=4;y2=4;
+	    dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));	 	    
+            System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
 
 	}
 
